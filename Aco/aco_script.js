96c112a98ba2413e5ba27dfd4eeb650c9eed19e6
Acoidán Betancor Arocha <acoarocha097@gmail.com>
2024-10-23 09:51:17 +0100

    Version Aco

---
+++ Aco/aco_script.js
@@ -0,0 +1,14 @@
+// concat()
+
+document.getElementById('btn_concat').addEventListener('click', function () {
+    // Variables
+    let string_uno = document.getElementById('string_uno').value;
+    let string_dos = document.getElementById('string_dos').value;;
+    let string_tres = document.getElementById('string_tres').value;;
+    let resultado_concat = "";
+    
+    // Alerta con la concatenación de los valores de los inputs
+    alert(`El String resultante tras concat() es: ` + resultado_concat.concat(string_uno, string_dos, string_tres));
+});
+
+// indexOf()
